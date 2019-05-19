124059e16f59b1673ec5ac00244ebf3d39548fa1
enzo aurichi batrov <enzo_batrov@hotmail.com>
2019-05-19 19:10:08 -0300

    Quick fix to get the duplicates only from the specific user

--- src/controllers/todoController.js
+++ src/controllers/todoController.js
@@ -6,7 +6,7 @@ const todoModel = mongoose.model('todo.todo');
 module.exports = {
     async createTodo(req, res){
         const allTodos = await todoModel.find({});
-        const duplicates = _.find(allTodos, {'title': req.body.title});
+        const duplicates = _.find(allTodos, {'title': req.body.title, 'userId': req.body.userId});
         if(!duplicates || duplicates.length > 0) {
             const created = await todoModel.create(req.body);
             res.status(200).json(created);
